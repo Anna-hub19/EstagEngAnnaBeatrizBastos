2f9b571dd038b0ba990ab1b852e3bfcf71114fa9
Anna Beatriz Bastos <127196700+Anna-hub19@users.noreply.github.com>
2024-08-15 12:56:35 -0300

    Update conexao-api.service.ts

--- src/app/service/conexao-api.service.ts
+++ src/app/service/conexao-api.service.ts
@@ -10,7 +10,7 @@ export class ConexaoApiService {
   constructor(private http: HttpClient) { }
   //validar o cpf
   ValidateCpf(cpf: string): Observable <any>{
-   const url = `http://localhost:5124/api/cpf/validate/${cpf}`
+   const url = `http://localhost:5124/cpf/validar/${cpf}`
    return this.http.get(url);
   }
 }
